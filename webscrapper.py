57057b90fa809cca1552e0e57ef5c1ae112eb460
garryc5 <33467416+garryc5@users.noreply.github.com>
2019-09-23 09:57:06 -0500

    fixed webbscrapper

--- webscrapper.py
+++ webscrapper.py
@@ -1,16 +1,16 @@
-from bs4 import BeautifulSoup
-import requests
-
-url = "https://www.ideafit.com/fitness-library"
-response = requests.get(url , timeout=5)
-content = BeautifulSoup(response.content, "html.parser")
-grabbedfeed = content.find_all("div", attrs={"class": "box white article teaser large wide nopad has-image clearfix"})
-toReturn = []
-for x in range(6):
-    artical = {
-        'img' : grabbedfeed[x].img,
-        'link' : url + grabbedfeed[x].h3.a.get('href'),
-        'headline' : grabbedfeed[x].h3.text,
-        'discript' : grabbedfeed[x].p
-    }
-    toReturn.append(artical)
+def webscrapper():
+    url = "https://www.ideafit.com/fitness-library"
+    response = requests.get(url, timeout=5)
+    content = BeautifulSoup(response.content, "html.parser")
+    grabbedfeed = content.find_all("div", attrs={
+                                   "class": "box white article teaser large wide nopad has-image clearfix"})
+    toReturn = []
+    for x in range(5):
+        artical = {
+            'img': grabbedfeed[x].img.get("src"),
+            'link': url + grabbedfeed[x].h3.a.get('href')[16:],
+            'headline': grabbedfeed[x].h3.text,
+            'discript': grabbedfeed[x].p.text
+        }
+        toReturn.append(artical)
+    return toReturn
